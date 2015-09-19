a1d23e0fd4f18410dc0f046896e4a45a1e4e235a
nirasan <nirasan@gmail.com>
2015-09-19 16:57:51 +0900

    商品の登録ができるように

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150919065708) do
+ActiveRecord::Schema.define(version: 20150919072956) do
 
   create_table "admins", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -39,6 +39,7 @@ ActiveRecord::Schema.define(version: 20150919065708) do
     t.integer  "show_priority"
     t.datetime "created_at",    null: false
     t.datetime "updated_at",    null: false
+    t.string   "image"
   end
 
   create_table "users", force: :cascade do |t|
