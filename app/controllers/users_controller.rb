ae05003851758eb12bc6f4c8e46f2f409d5ce85c
nirasan <nirasan@gmail.com>
2015-09-26 00:31:35 +0900

    配送先とプロフィール編集の追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,8 +9,21 @@ class UsersController < ApplicationController
 
   def update_profile
     respond_to do |format|
-      if @user.update(params.require(:user).permit(:name, :image))
-        format.html { redirect_to user_path(@user), notice: 'User was successfully updated.' }
+      if current_user.update(params.require(:user).permit(:name, :image))
+        format.html { redirect_to users_path(@user), notice: 'プロフィールを更新しました' }
+      else
+        format.html { render :profile }
+      end
+    end
+  end
+
+  def delivery
+  end
+
+  def update_delivery
+    respond_to do |format|
+      if current_user.update(params.require(:user).permit(:address, :zip_code, :phone_number))
+        format.html { redirect_to users_path(@user), notice: '配送先を更新しました' }
       else
         format.html { render :profile }
       end
